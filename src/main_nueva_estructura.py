d7672dde63968a0d8b73ae36ed60cbc67873b784
Danny Garzón <degarzonm@unal.edu.co>
2021-07-14 00:39:09 -0500

    reading csv

--- src/main_nueva_estructura.py
+++ src/main_nueva_estructura.py
@@ -1,6 +1,6 @@
 from os import name
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import Label, filedialog
 from tkinter.constants import COMMAND
 import math
 import numpy as np
@@ -8,8 +8,12 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
+import csv
 
 class Application(tk.Frame):
+    counter=0
+    
+
     def __init__(self, master=None):
         tk.Frame.__init__(self,master)
         self.createWidgets()
@@ -24,7 +28,10 @@ class Application(tk.Frame):
 
         #fig=plt.figure(figsize=(8,8))
         fig = Figure(figsize=(7,5), dpi=100)
-        ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+        
+        #ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+        ax=fig.add_axes([0.05,0.05,1,1])
+        
         #canvas=FigureCanvasTkAgg(fig,master=root)
         #canvas.get_tk_widget().grid(row=0,column=1)
         #canvas.draw()
@@ -39,34 +46,45 @@ class Application(tk.Frame):
         toolbar.update()
 
         self.titulos=tk.Label(master=root, text="Estimador de densidad con Kernel", font = "Helvetica 16 bold")
-        self.titulos.grid(sticky=tk.W, pady=4, padx=5)
+        self.titulos.grid(sticky=tk.E, pady=4, padx=5)
         #Elemento visual - botones
 
         self.boton_archivo = tk.Button(master=root, text="archivo CSV", command=lambda: self.selectorArchivo())
-        self.boton_archivo.grid(row=1, column=3)
+        self.boton_archivo.grid(row=1, column=3,sticky=tk.E+tk.W+tk.S+tk.N)
 
         self.boton_graficar=tk.Button(master=root, text="Graficar", command=lambda: self.graficar(lienzo,ax))
-        self.boton_graficar.grid(row=2, column=3, pady=4)
+        self.boton_graficar.grid(row=2, column=3, pady=4,sticky=tk.E+tk.W+tk.S+tk.N)
 
-        self.boton_randomize=tk.Button(master=root, text="Mezclar",command=self.actualizarDatos)
-        self.boton_randomize.grid(row=3, column=3,pady=4)
+        self.boton_randomize=tk.Button(master=root, text="Mezclar",command=lambda: self.actualizarDatos(fig,ax))
+        self.boton_randomize.grid(row=3, column=3,pady=4,sticky=tk.E+tk.W+tk.S+tk.N)
 
-        
+    
     def graficar(self,canvas,ax):
         c = ['r','b','g']  # plot marker colors
         ax.clear()         # clear axes from previous plot
         for i in range(3):
-            theta = np.random.uniform(0,360,10)
+            theta = np.random.uniform(0,36,10)
             r = np.random.uniform(0,1,10)
             ax.plot(theta,r,linestyle="None",marker='*', color=c[i])
             canvas.draw()
     
+    #cargamos el dataset en un csv globlal 
     def selectorArchivo(self):
+        
         global archivo_csv
         self.filename = filedialog.askopenfilename(initialdir="/datasets", title="Seleccione el archivo de datos separado por coma",filetypes=(("archivos csv","csv"),("todos","*")))
+        texto_ruta_csv= Label(root , text= self.filename).grid(row=4, column=3,pady=4)
+
+        
+        result = np.genfromtxt(self.filename,delimiter=',',skip_header=1,dtype=None,encoding=None)
+
     
-    def actualizarDatos():
-        print("To Do: refrescar")
+    def actualizarDatos(self,fig: Figure,ax):
+        #ax.delaxes()
+        #ax=fig.add_axes([0.2,0.1,0.8,0.8])
+        self.counter+=1
+        print("To Do: refrescar", self.counter)
+        print("Aqui quiero actualizar la grafica, dada una columna", self.filename)
 
 root=tk.Tk()
 app=Application(master=root)
